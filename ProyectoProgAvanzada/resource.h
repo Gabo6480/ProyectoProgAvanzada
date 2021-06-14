8ce7198ed85d8c2bae86713d52fd758d6f37159d
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2021-06-14 13:21:48 -0500

    Initial commit

---
+++ ProyectoProgAvanzada/resource.h
@@ -0,0 +1,87 @@
+//{{NO_DEPENDENCIES}}
+// Archivo de inclusión generado de Microsoft Visual C++.
+// Usado por ProyectoProgAvanzada1.rc
+//
+#define IDD_VENTAGRE                    101
+#define IDD_PRINCIPAL                   103
+#define IDR_MENU1                       107
+#define IDD_LISTACONTA                  108
+#define IDD_DIALOGADDGRP                110
+#define IDD_VENTAVER                    111
+#define IDD_DIALOGGRUPOSVER             112
+#define IDR_MENU2                       114
+#define IDC_STATICDATOS                 1001
+#define IDC_STATICDIREC                 1002
+#define IDC_STATICONTAC                 1003
+#define IDC_EDITNBE                     1004
+#define IDC_EDITAPO                     1005
+#define IDC_STATICFOTOC                 1006
+#define IDC_EDITMIL                     1007
+#define IDC_DATETIMEPICKERCUMPLE        1008
+#define IDC_EDITCUMPLE                  1008
+#define IDC_EDITTEL                     1009
+#define IDC_COMBOGRUPO                  1010
+#define IDC_EDITGRPO                    1012
+#define IDC_EDITCALLE                   1014
+#define IDC_EDITNUM                     1015
+#define IDC_EDITCODP                    1016
+#define IDC_EDITBTWCALL                 1017
+#define IDC_COMBOCOLON                  1018
+#define IDC_COMBOMUNIC                  1019
+#define IDC_COMBOESTADO                 1020
+#define IDC_COMBOPAIS                   1021
+#define IDC_EDITPAIS                    1022
+#define IDC_EDITCOLON                   1023
+#define IDC_EDITMUNIC                   1024
+#define IDC_STATICNBE                   1025
+#define IDC_STATICAPO                   1026
+#define IDC_EDITESTADO                  1027
+#define IDC_STATICMAIL                  1028
+#define IDC_STATICCUMPLE                1029
+#define IDC_STATICTEL                   1030
+#define IDC_STATICGRUPO                 1032
+#define IDC_STATICCALLE                 1033
+#define IDC_STATICNUM                   1034
+#define IDC_STATICCODP                  1035
+#define IDC_STATICBTCALL                1036
+#define IDC_STATICPAIS                  1038
+#define IDC_STATICCOLON                 1039
+#define IDC_STATICMUNIC                 1040
+#define IDC_STATICESTADO                1041
+#define IDC_BUTTIZQ                     1042
+#define IDC_BUTTADD                     1043
+#define IDC_BUTTDER                     1044
+#define IDC_STATICINDICE                1045
+#define IDC_STATICENTRE                 1046
+#define IDC_BUTTDELETE                  1052
+#define IDC_EDITSEARCH                  1054
+#define IDC_EDITGRPNBE                  1057
+#define IDC_LISTGRUPOS                  1060
+#define IDC_LISTINTEGRANTES             1061
+#define IDC_BUTTDEL                     1063
+#define ID_OPCIONES_COLORDEPANTALLA     40001
+#define ID_MEN40002                     40002
+#define ID_OPCIONES_COLORDEVENTANA      40003
+#define ID_MEN40004                     40004
+#define ID_GRUPOS_CREARGRUPO            40005
+#define ID_GRUPOS_VERGRUPOS             40006
+#define ID_MEN40007                     40007
+#define ID_CONTACTOS_AGREGARCONTACTO    40008
+#define ID_CONTACTOS_VERCONTACTOS       40009
+#define ID_CONTACTO_EDITAR              40010
+#define ID_CONTACTO_ELIMIN              40011
+#define ID_OPCIONES_SALIR               40012
+#define ID_OPCIONES_SALIR40013          40013
+#define ID_GRUPOS_CREARGRUPO40014       40014
+#define ID_GRUPOS_VERGRUPOS40015        40015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        115
+#define _APS_NEXT_COMMAND_VALUE         40016
+#define _APS_NEXT_CONTROL_VALUE         1064
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
